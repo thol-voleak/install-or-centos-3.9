f7f6bf67011ee19f60587ab8df87eadaa5efb3f1
thol-voleak <voleak.rupp@gmail.com>
2018-03-12 15:52:57 +0700

    update

--- install-openshift.sh
+++ install-openshift.sh
@@ -6,7 +6,7 @@ echo "------------>1.process export variable------------>"
 export DOMAIN=${DOMAIN:="192.168.106.2"}
 export USERNAME=${USERNAME:="voleak"}
 export PASSWORD=${PASSWORD:=voleak}
-export VERSION=${VERSION:="v3.6.0"}
+export VERSION=${VERSION:="v3.7.1"}
 
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/thol-voleak/openshift-origin-centos-installation/master"}
 
